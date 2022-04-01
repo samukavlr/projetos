a250b97e8bd2caf297c32878c8402bb287ca569a
SAMUEL MARCIANO <samuel.marciano@senacsp.edu.br>
2022-03-31 21:45:25 -0300

    exemplo de useStatus

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import {useState} from 'react';
 import './App.css';
 import Header from './Header';
 
@@ -9,17 +10,26 @@ function App() {
   function soma(a,b){
     return a+b
   }
+  const [contador, setContador]= useState(0);
+  //let contador= 0
+  function handleButtonContar(){
+   setContador (contador+1);
+   
+  }
   return (
-    <>
+     <div className="App">
       <Header title="meu primeiro componente React"/>
       <Header title="um componente props"/>
       <h2>
         Senac Campinas
         <div>Retorno da Soma é : <br />{soma(10,20)}</div>
       </h2> 
-      <button onClick={handleButtonClick}>Click me! </button>     
+      <button onClick={handleButtonClick}>Click me! </button> 
+      <Header title={contador}/> 
+      <button onClick={handleButtonContar}>contar </button> 
       
-    </>
+      
+     </div>
   );
 }
 
